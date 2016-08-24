53b721b0e4b8372d1a6eb5a30d5b9820cba9f40d
Nekic <zhao582059960@gmail.com>
2016-08-24 17:15:12 +0800

    入口文件和常量定义以及目录结构

---
+++ Framework/Framework.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Framework;
+
+/**
+ * Class Framework
+ * 框架启动类
+ */
+class Framework
+{
+    /**
+     * 启动框架
+     */
+    public static function run()
+    {
+        echo 'Ok';
+    }
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 入口文件
+ * 1.定义常量
+ * 2.加载函数库
+ * 3.启动框架
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__). DS );
+define('FRAMEWORK', ROOT . 'Framework' . DS);
+define('COMMON', FRAMEWORK . 'Common' . DS);
+define('APP', ROOT . 'Application' . DS);
+
+// 是否开启调试模式
+define('DEBUG', true);
+
+if (DEBUG) {
+    ini_set('display_error', 'On');
+} else {
+    ini_set('display_error', 'Off');
+}
+
+// 加载函数库
+include_once COMMON . 'function.php';
+
+// 加载框架启动类
+require_once FRAMEWORK . 'Framework.class.php';
+
+// 启动框架
+\Framework\Framework::run();
